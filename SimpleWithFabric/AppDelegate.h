34e74590a8b5100010b516f71ffbdbf90605f90b
Mia <mia.shopgal@gmail.com>
2015-08-06 11:18:09 +0800

    Initial Commit

---
+++ SimpleWithFabric/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleWithFabric
+//
+//  Created by Mia Yu on 8/6/15.
+//  Copyright (c) 2015 Mia Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleWithFabric/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleWithFabric
+//
+//  Created by Mia Yu on 8/6/15.
+//  Copyright (c) 2015 Mia Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
